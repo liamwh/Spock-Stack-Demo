14858a04a9d018459ebad771213a5842af8577b8
Liam Woodleigh-Hardinge <liam@liamwh.com>
2023-01-01 21:34:38 +1100

    Only display messages if logged in

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import Login from "./lib/Login.svelte";
   import Messages from "./lib/Messages.svelte";
+  import { currentUser } from "./lib/pocketbase";
 </script>
 
 <h1>Pocketchat</h1>
 
 <Login />
-<Messages />
+{#if $currentUser}
+  <Messages />
+{/if}
